4011b9576bd0ecb3e5771af9ee215fd5069ad4b5
Cordelia <c.mcgeetubb@salesforce.com>
2020-01-29 17:36:38 -0800

    initial poll commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import Poll from './components/Poll';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <button className="slds-button slds-button_neutral">Click </button>
+      <Poll></Poll>
     </div>
   );
 }
