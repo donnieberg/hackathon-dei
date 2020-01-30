5a1216c310fbda890d1e9011580d52c99e878fcf
Donielle Berg <donielle.berg@gmail.com>
2020-01-29 16:41:43 -0800

    Add in tabset functionality

--- src/components/post.js
+++ src/components/post.js
@@ -9,11 +9,12 @@ class Post extends React.Component {
 
   render() {
     return (
-      <div className="slds-form-element">
+      <div className="slds-form-element cf">
         <label className="slds-form-element__label" htmlFor="textarea-id-01">Share an Update</label>
         <div className="slds-form-element__control">
           <textarea id="textarea-id-01" className="slds-textarea" placeholder="Write here..."></textarea>
         </div>
+        <button className="slds-button slds-button_brand fr mts">Share</button>
       </div>
     );
   }
