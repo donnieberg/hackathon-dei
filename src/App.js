f073531e4929b64aa1f4e3c1d479b2bde88653ea
Donielle Berg <donielle.berg@gmail.com>
2020-01-29 21:19:43 -0800

    Add side panel and header avatar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
+
 import GlobalHeader from './components/globalHeader';
+import SidePanel from './components/sidePanel';
+
 import Poll from './components/poll';
 import Tabset from './components/tabset';
 import Post from './components/post';
@@ -11,20 +14,25 @@ function App() {
   return (
     <div className="App">
       <GlobalHeader />
-      <div className="width-tabs center-horz">
-        <Tabset 
-          tabs={[
-          <Post label="Post" />,
-          <Question label="Question" />,
-          <Poll label="Poll" />
-          ]}
-        />
-        <ul className="pvl phm list-plain list-horz bg-white">
-          <li><FeedItem /></li>
-          <li><FeedItem /></li>
-          <li><FeedItem /></li>
-          <li><FeedItem /></li>
-        </ul>
+      <div className="width-main center-horz cf">
+        <div className="fl width-tabs prl">
+          <Tabset 
+            tabs={[
+            <Post label="Post" />,
+            <Question label="Question" />,
+            <Poll label="Poll" />
+            ]}
+          />
+
+          <div className="brt-s pam fw-bold slds-text-title_caps bg-white">Feed</div>
+          <ul className="brb-s pvl phm list-plain list-horz bg-white">
+            <li><FeedItem /></li>
+            <li><FeedItem /></li>
+            <li><FeedItem /></li>
+            <li><FeedItem /></li>
+          </ul>
+        </div>
+        <SidePanel />
       </div>
     </div>
   );
